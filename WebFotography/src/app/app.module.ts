228286930ad0e0c208bcf8a99bd5f27e61b95032
lorenzogalve <lorencd23@gmail.com>
2023-11-21 00:36:17 +0100

    footer y arreglos
    -Añadido footer con instagram y linkedin
    -Arreglos en el listado de imagenes con respecto a los textos

--- WebFotography/src/app/app.module.ts
+++ WebFotography/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { ListImagesComponent } from './components/list-images/list-images.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    ListImagesComponent
+    ListImagesComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
